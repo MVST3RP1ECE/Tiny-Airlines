b56519dae7aa398503c3f9ee0e6b179060cd942a
Aleksandr Khudoleev <mr.sasha.qwe@gmail.com>
2024-09-10 13:34:04 +0300

    finished <Header> component. Created 3 main colors, added font Montserrat Alternates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Header from './Components/Header.jsx/Header'
+import Header from './Components/Header/Header'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <Header/>
---
+++ src/Components/Button/ButtonSVG.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+export default function Button({icon, text}) {
+
+  function nameLog(){
+    console.log(text);
+  }
+
+  // TODO разобраться с подключением шрифтов к react проекту. Приступить к написанию след. секции
+
+  if(text == "Профиль"){
+    return(
+      <button className='button_profile' onClick={nameLog}>
+        <div className='svg_wrapper'>
+            {icon}
+        </div>
+        <p className='plain_text montserrat-alternates-regular '>
+            {text}
+        </p>
+     </button>
+    )
+  }
+  else if( text == "Поддержка"){
+    return (
+      <button className='button_support' onClick={nameLog}>
+          <div className='svg_wrapper'>
+              {icon}
+          </div>
+          <p className='plain_text montserrat-alternates-regular '>
+              {text}
+          </p>
+       </button>
+    )
+  }
+  return (
+    <button className='button_cur' onClick={nameLog}>
+        <div className='svg_wrapper'>
+            {icon}
+        </div>
+        <p className='plain_text montserrat-alternates-regular '>
+            {text}
+        </p>
+     </button>
+  )
+}
